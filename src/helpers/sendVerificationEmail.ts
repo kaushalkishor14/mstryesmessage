2601025ca591398df5bae34cd32951c17d40a70e
kaushal <kaushalkishorroy21@gmail.com>
2024-04-14 23:36:29 +0530

    emailroutes

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,1 +1,26 @@
 import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/verififcationEmail";
+
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail(
+
+    emails:string,
+    username:string,
+    verifyCode:string
+
+):Promise<ApiResponse>{
+    try {
+        await resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: 'email',
+            subject: 'mstryesmessage | Verification code',
+            react: VerificationEmail({username, otp:verifyCode}),
+          });
+        return {success:true,message:"verification eamil send successfully"}
+    } catch (emailerror) {
+        console.error("Error sending verification email",emailerror)
+        return {success:false,message:"failed to send verification eamil"}
+        
+    }
+}
