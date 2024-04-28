1e0371382486603b0c7be17a90bea1614dc03af3
kaushal <kaushalkishorroy21@gmail.com>
2024-04-28 20:23:54 +0530

    verify user done

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,5 +1,6 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
+import { use } from "react";
 
 
 export async function POST(request: Request){
@@ -8,7 +9,53 @@ export async function POST(request: Request){
     try {
     const {username, code} =   await request.json()
 
-    decodeURIComponent(username)
+   const decodedUsername = decodeURIComponent(username)
+const user = await UserModel.findOne({username: decodedUsername})
+
+if(!user){
+    return Response.json(
+        {
+            success: false,
+            message:"User not found"
+        },
+        {status:500}
+    )
+}
+
+const isCodeVaild = user.verifyCode === code
+const isCodeExpire = new Date(user.verifyCodeExpiry) > new Date()
+
+if(isCodeVaild && isCodeExpire){
+    user.isVerified == true
+    await user.save()
+
+    return Response.json(
+        {
+            success: true,
+            message:"Account verified successfully"
+        },
+        {status:200}
+    )
+} else if (!isCodeExpire){
+    return Response.json(
+        {
+            success: false,
+            message:"Verification code has expired"
+        },
+        {status:400}
+    )
+
+} else{
+    return Response.json(
+        {
+            success: false,
+            message:"Incorrect Verification code"
+        },
+        {status:400}
+    )
+
+}
+
 
     } catch (error) {
         console.error("Error verifying  username", error)
