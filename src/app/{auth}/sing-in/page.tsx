4debd25d89aaad7ac598d29ebe7dfcba09a20520
kaushal <kaushalkishorroy21@gmail.com>
2024-05-03 23:57:34 +0530

    ui doing

--- src/app/{auth}/sing-in/page.tsx
+++ src/app/{auth}/sing-in/page.tsx
@@ -1,13 +1,28 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 'use client '
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z  from "zod"
 import Link from "next/link"
+import { useState } from "react"
+import { useDebounceValue } from 'usehooks-ts'
+import { useToast } from "@/components/ui/use-toast"
+import { useRouter } from "next/navigation"
+
 
  const page = () => {
 
-  const[name, setName]
+  const[username, setUsername] = useState('');
+  const[ usernameMessage, setUsernameMessage] = useState('');
+  const[isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const[isSubmting, setIsSubmitinhg ] = useState(false)
+
+  const debouncedUsername =useDebounceValue(username,300);
+
+  const { toast } = useToast();
+  const router = useRouter();
+
 
 
   return (
