abd731f5611fe350c751d73164a12d28912f8643
kaushal <kaushalkishorroy21@gmail.com>
2024-05-08 22:55:04 +0530

    handling dashboard logic

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+'use client'
 
-export const page = () => {
+import { useState } from "react";
+import {Message} from "@/model/User"
+import { useToast } from "@/components/ui/use-toast";
+import { useSession } from "next-auth/react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AcceptMessageSchema } from "@/schemas/acceptMessageSchema";
+
+const page = () => {
+
+  const [messages ,setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSwitchLoading , setIsSwitchLoading] = useState(false)
+
+
+  const {toast} = useToast()
+
+  const handleDeleteMessage = (messageId: string) =>{
+    setMessages(messages.filter((message)=> message.id !== messageId))
+  }
+
+  const {data: session } = useSession()
+
+  const form = useForm({
+    resolver: zodResolver(AcceptMessageSchema)
+  })
+
+  const {register, watch , setValue} = form;
+
+  const acce
+
+
+
+
   return (
     <div>dashboard</div>
   )
 }
+
+
+export default page;
