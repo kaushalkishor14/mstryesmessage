31239cc8208d3aee09d50513429cb6a6950474b9
kaushal <kaushalkishorroy21@gmail.com>
2024-05-13 20:45:48 +0530

    dashboard done

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -10,6 +10,10 @@ import { AcceptMessageSchema } from "@/schemas/acceptMessageSchema";
 import axios, { AxiosError } from "axios";
 import { ApiResponse } from "@/types/ApiResponse";
 import { User } from "next-auth";
+import { Button } from "@/components/ui/button";
+import { Loader2, RefreshCcw } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { Separator } from "@/components/ui/separator";
 
 const page = () => {
 
@@ -128,14 +132,87 @@ const page = () => {
   }
 
  const {username}= session?.user as User
+
+ const baseUrl =`${window.location.protocol} //${window.location.host}`
+ const profileUrl = `${baseUrl}/u//${username}`
+
+
+ const copyToClipboard = () =>{
+
+  navigator.clipboard.writeText(profileUrl)
+  toast({
+    title: "Url copied",
+    description: "profileUrl is copied to chilpboard"
+  })
   
+
+ }
+  
+ 
+
 
   if(!session || !session.user){
     return <div>PLease login</div>
   }
 
   return (
-    <div>dashboard</div>
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+    <h1 className="text-4xl font-bold mb-4">User Dashboard</h1>
+
+    <div className="mb-4">
+      <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>{' '}
+      <div className="flex items-center">
+        <input
+          type="text"
+          value={profileUrl}
+          disabled
+          className="input input-bordered w-full p-2 mr-2"
+        />
+        <Button onClick={copyToClipboard}>Copy</Button>
+      </div>
+    </div>
+
+    <div className="mb-4">
+      <Switch
+        {...register('acceptMessages')}
+        checked={acceptMessages}
+        onCheckedChange={handleSwitchChange}
+        disabled={isSwitchLoading}
+      />
+      <span className="ml-2">
+        Accept Messages: {acceptMessages ? 'On' : 'Off'}
+      </span>
+    </div>
+    <Separator />
+
+    <Button
+      className="mt-4"
+      variant="outline"
+      onClick={(e) => {
+        e.preventDefault();
+        fetchMessages(true);
+      }}
+    >
+      {isLoading ? (
+        <Loader2 className="h-4 w-4 animate-spin" />
+      ) : (
+        <RefreshCcw className="h-4 w-4" />
+      )}
+    </Button>
+    <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+      {/* {messages.length > 0 ? (
+        messages.map((message, index) => (
+          <MessageCard
+            key={message._id}
+            message={message}
+            onMessageDelete={handleDeleteMessage}
+          />
+        ))
+      ) : (
+        <p>No messages to display.</p>
+      )} */}
+    </div>
+  </div>
   )
 }
 
