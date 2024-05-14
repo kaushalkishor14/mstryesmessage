71425759ff4631af08bec1385ea61bb60f005ee9
kaushal <kaushalkishorroy21@gmail.com>
2024-05-14 20:59:23 +0530

    doing navbar

---
+++ src/context/ui/Navbar.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Link from "next/link"
+import { useSession, signOut } from "next-auth/react"
+import { User } from "next-auth"
+import { Button } from "@/components/ui/button"
+
+const Navbar = () => {
+
+
+    const { data: session } = useSession()
+
+    const user: User = session?.user
+
+
+    return (
+        <nav>
+            <div>
+                <a href="#"> Mystry Message</a>
+                {
+                    session ? (
+                        <>
+                            <span>Welcome , {user.username || user.email}
+                            </span>
+                            <Button onClick={() => signOut()}>Logout</Button>
+                        </>
+                    ) : (
+                        <link href="/sign-in">Login</link>
+                    )
+                }
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
