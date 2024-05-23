767d091e4204c06f293f7fbef1ba4b2795fd2793
kaushal <kaushalkishorroy21@gmail.com>
2024-05-23 11:28:23 +0530

    navbar messagecard

--- src/context/ui/Navbar.tsx
+++ src/context/ui/Navbar.tsx
@@ -14,18 +14,20 @@ const Navbar = () => {
 
 
     return (
-        <nav>
-            <div>
-                <a href="#"> Mystry Message</a>
+        <nav className="p-4 md:p-6 shadow-md">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <a href="#" className="text-xl font-bold mb-4 md:mb-0"> Mystry Message</a>
                 {
                     session ? (
                         <>
-                            <span>Welcome , {user.username || user.email}
+                            <span className="mr-4">Welcome , {user.username || user.email}
                             </span>
-                            <Button onClick={() => signOut()}>Logout</Button>
+                            <Button className="w-full md:w-auto" onClick={() => signOut()}>Logout</Button>
                         </>
                     ) : (
-                        <link href="/sign-in">Log-in</link>
+                        <Link href="/sign-in">
+                            <Button className="w-full md:w-auto">Login</Button>
+                        </Link>
                     )
                 }
             </div>
