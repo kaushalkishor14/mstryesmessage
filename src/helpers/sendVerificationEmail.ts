91da5e9173ac74a64a4c682a3ab855239e886726
kaushal <kaushalkishorroy21@gmail.com>
2024-09-03 12:59:59 +0530

    done

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,10 +9,11 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'dev@hiteshchoudhary.com',
+      from: 'kaushalkishorroy14@gmail.com',
       to: email,
       subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
+    
     });
     return { success: true, message: 'Verification email sent successfully.' };
   } catch (emailError) {
